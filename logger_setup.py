8a73cd2508c6f3ae50a9b5db29ee9db52316a424
munlicode <nurzhanmuratkhan@gmail.com>
2025-08-30 12:57:09 +0500

    Update of logger

--- logger_setup.py
+++ logger_setup.py
@@ -22,7 +22,10 @@ def setup_logger():
 
     # Create a rotating file handler
     handler = RotatingFileHandler(
-        log_file, maxBytes=max_log_size_mb * 1024 * 1024, backupCount=backup_count
+        log_file,
+        maxBytes=max_log_size_mb * 1024 * 1024,
+        backupCount=backup_count,
+        encoding="utf-8",
     )
 
     # Create a formatter and set it for the handler
